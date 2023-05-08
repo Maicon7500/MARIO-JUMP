8409d0c8a07e6c412ae990eb1b0e5c95321a60dc
Maicon7500 <124109768+Maicon7500@users.noreply.github.com>
2023-05-08 12:33:08 -0300

    Adicionado pular ao clicar na tela pelo celular.
    Codigo: document.addEventListener('click', jump);

--- js/script.js
+++ js/script.js
@@ -1,89 +1,40 @@
-* {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
+const mario = document.querySelector('.mario');
+
+const pipe = document.querySelector('.pipe');
+
+const jump = () => {
+    mario.classList.add('jump');
+
+    setTimeout(() =>{
+        mario.classList.remove('jump');
+    }, 500);
 }
 
-.game-board{
-    width: 100%;
-    height: 500px;
-    border-bottom: 15px solid rgb(16, 242, 16);
-    margin: 0 auto;
-    position: relative;
-    overflow: hidden;
-    background: linear-gradient(#87CEEB, #E0F6ff);
-}
+const loop = setInterval(() => {
 
-.pipe {
-    position: absolute;
-    bottom: 0;
-    width: 80px;
-    animation: pipe-animation 1.2s infinite linear;
-}
+    const pipePosition = pipe.offsetLeft;
+    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
+    
+    console.log(marioPosition);
 
-.mario {
-    width: 150px;
-    position: absolute;
-    bottom: 0;
-}
+    if(pipePosition <= 120 && pipePosition > 0 && marioPosition < 80){
 
-.jump{
-    animation: jump 500ms ease-out;
+        pipe.style.animation = 'none';
+        pipe.style.left = `${pipePosition}px`;
+   
+        mario.style.animation = 'none';
+        mario.style.bottom = `${marioPosition}px`;
+        
+        mario.src = './images/game-over.png';
+        mario.style.width = '115px';
+        margin.style.marginLeft = '50px';
 
-}
+        mario.src = './images/clouds.png';
 
-.clouds{
-    position: absolute;
-    width: 400px;
-    animation: clouds-animation 20s infinite linear;
-
-}
-
-@keyframes pipe-animation {
-
-    from{
-        right: -80px;
-    }
-to{
-    right: 100%;
-}
-}
-
-@keyframes jump{
-    0%{
-        bottom: 0;
+        clearInterval(loop);
     }
 
-    40% {
-        bottom:180px;
-    }
+}, 10);
 
-    60%{
-        bottom: 180px;
-    }
-
-    100% {
-        bottom: 0;
-    }
-
-}
-
-@keyframes clouds-animation{
-    from{
-        right: -400px;
-    }
-
-    to{
-        right:100%
-    }
-}
-
-const gameBoard = document.querySelector('.game-board');
-
-gameBoard.addEventListener('touchstart', () => {
-  const mario = document.querySelector('.mario');
-  mario.classList.add('jump');
-  setTimeout(() => {
-    mario.classList.remove('jump');
-  }, 500);
-});
+document.addEventListener('click', jump);
+document.addEventListener('keydown', jump);
